ba6194bdbcab973336f21af5523c8e3445563bb3
trungkien1812 <nguyenhuutrungkien1812@gmail.com>
2023-06-25 22:54:13 +0700

    lab05 upadated

--- GUIProject/hust/soict/globalict/javafx/Painter.java
+++ GUIProject/hust/soict/globalict/javafx/Painter.java
@@ -1,4 +1,25 @@
 package GUIProject.hust.soict.globalict.javafx;
 
-public class Painter {
+import com.sun.glass.ui.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Painter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass()
+                .getResource("/hust/soict/globalict/javafx/Painter.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Painter");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
 }
